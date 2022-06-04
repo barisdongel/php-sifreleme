4bd64acc2f510056f8acb10ac9a295d294430cbc
barisdongel <barisdng26@gmail.com>
2022-06-04 11:49:16 +0300

    crypting end

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
 
-function sifreleme($cryp, $key)
+function crypting($cryp, $key)
 {
     $ch = str_split(strtolower($cryp));
+    $kk = str_split(strtolower($key));
 
     $arr = array("a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6, "g" => 7, "h" => 8, "i" => 9, "j" => 10, "k" => 11, "l" => 12, "m" => 13, "n" => 14, "o" => 15, "p" => 16, "q" => 17, "r" => 18, "s" => 19, "t" => 20, "u" => 21, "v" => 22, "w" => 23, "x" => 24, "y" => 25, "z" => 26);
 
+    //key degerini alma
+    for ($j = 0; $j < count($kk); $j++) {
+        //keyi parcalama
+        $equal = strtr($kk[$j], $arr) . '-';
+        $keycode = explode("-", $equal);
+    }
 
     for ($i = 0; $i < count($ch); $i++) {
         //sifreyi parcalama
@@ -13,9 +20,10 @@ function sifreleme($cryp, $key)
         $explode = explode("-", $equal);
 
         //sifreyi key ile toplama
-        $topla = $explode[$i] + $key;//key ile toplanacak;
-        $coz = array_search ($topla, $arr);
+        $topla = fmod(($explode[$i] + $keycode[$i]), 26); //şifre de ki harflere karşılık gelen sayı key de ki harflere karşılık gelen sayı ile toplanıp 25 ile mod'u alınacak;
+        $coz = array_search($topla, $arr);
         print_r($coz);
+        echo "<br>";
     }
 }
 
@@ -26,11 +34,11 @@ function executeLetters($pass, $keyvalue)
     $input = str_split($pass);
 
     foreach ($input as $p) {
-        $return .= sifreleme($p, $keyvalue);
+        $return .= crypting($p, $keyvalue);
     }
 }
 
-$sifre = "Baris";
-$anahtar = 4;
+$sifre = "baris";
+$anahtar = "abc";
 
 $sifrelimetin = executeLetters($sifre, $anahtar);
