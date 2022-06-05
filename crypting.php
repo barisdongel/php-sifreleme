d9e3c470df95dbe4ac05ebb9f59bc6346a8f0579
barisdongel <barisdng26@gmail.com>
2022-06-05 03:25:35 +0300

    realy last update .d

--- crypting.php
+++ crypting.php
@@ -1,14 +1,15 @@
 <?php
-if(isset($_POST['encrypt'])){
+if(isset($_POST['encrypt'])) {
     $originpass = $_POST['password']; //şifremiz
     $originkey = $_POST['key']; //key değerimiz
-    
+
     $ch = str_split($originpass); //şifreyi parçalıyoruz
     $key = str_split($originkey); //keyi parçalıyoruz
-    
-    $a = range(1, 26); //1'den 26'ya kadar olan bir dizi
+
     $b = range('a', 'z'); //a'dan z'ye kadar olan bir dizi
-    $c = array_map(null, $a, $b); //$a ve $b dizisinin array_map ile denkleştirilmesi
+    array_unshift($b, "phoney"); //$b dizisinin indisini 1'den başlatmak için boş eleman ekleyip 1.indisi siliyoruz
+    unset($b[0]);
+
     for ($i = 0; $i < 26; $i++) { //26 kere dön
         if (!array_key_exists($i, $ch)) { //eğer $i değeri sifre uzunluğundan büyükse döngüden çık. (key harf sayısı kadar dönüyor döngü)
             break;
@@ -17,13 +18,12 @@ if(isset($_POST['encrypt'])){
             $crypt = $d[$i][1]; //$d dizisinin tek tek her elemanından key değerine karşılık gelen harfi alıyoruz
             $keyvalue = (array_search($crypt, $b)); //harf dizimizde key değerini arıyoruz. dönen yanıt karşılık gelen value değerinin sayısı
             $passvalue = (array_search($ch[$i], $b)); // harf dizimizde şifre değerini arıyoruz. dönen yanıt karşılık gelen value değerinin sayısı
-    
-            $equal[] = (int)(fmod(($passvalue + $keyvalue), 26) + 1); //şifreden dönen sayı ile keyden dönen sayıyı toplayıp 26'ya göre modunu alıyoruz ve integer a çevirip $equal dizisine ekliyoruz.
-            @$pass[] = $b[$equal[$i]];//key değerinde ki harflere karşılık gelen sayıları, $b yani harfler dizimizde ki keylere denk gelen harflerle eşleştiriyoruz. Ve eşleşen harfler şifrelenmiş metnimiz oluyor
+            $equal[] = (int)(fmod(($passvalue + $keyvalue), 26)+1); //şifreden dönen sayı ile keyden dönen sayıyı toplayıp 26'ya göre modunu alıyoruz ve integer a çevirip $equal dizisine ekliyoruz.
+
+            $pass[] = $b[$equal[$i]]; //key değerinde ki harflere karşılık gelen sayıları, $b yani harfler dizimizde ki keylere denk gelen harflerle eşleştiriyoruz. Ve eşleşen harfler şifrelenmiş metnimiz oluyor
         }
     }
-    $pass = (implode($pass)); //sifre dizisini string'e çeviriyoruz
-    
 
-    header("Location:index.php?encryptedtext=".base64_encode($pass));
+    $pass = (implode($pass)); //sifre dizisini string'e çeviriyoruz
+    header("Location:index.php?encryptedtext=" . base64_encode($pass));
 }
--- decrypting.php
+++ decrypting.php
@@ -6,10 +6,10 @@ if(isset($_POST['decrypt'])){
     $ch = str_split($originpass); //metni parçalıyoruz
     $key = str_split($originkey); //keyi parçalıyoruz
     
-    $a = range(1, 26); //1'den 26'ya kadar olan bir dizi
     $b = range('a', 'z'); //a'dan z'ye kadar olan bir dizi
-    $c = array_map(null, $a, $b); //$a ve $b dizisinin array_map ile denkleştirilmesi
-    for ($i = 0; $i < 25; $i++) { //26 kere dön
+    array_unshift($b , "z"); //$b dizisinin indisini 1'den başlatmak için boş eleman ekleyip 1.indisi siliyoruz
+
+    for ($i = 0; $i < 26; $i++) { //26 kere dön
         if (!array_key_exists($i, $ch)) { //eğer $i değeri sifre uzunluğundan büyükse döngüden çık. (key harf sayısı kadar dönüyor döngü)
             break;
         } else {
@@ -17,13 +17,16 @@ if(isset($_POST['decrypt'])){
             $crypt = $d[$i][1]; //$d dizisinin tek tek her elemanından key değerine karşılık gelen harfi alıyoruz
             $keyvalue = (array_search($crypt, $b)); //harf dizimizde key değerini arıyoruz. dönen yanıt karşılık gelen value değerinin sayısı
             $passvalue = (array_search($ch[$i], $b)); // harf dizimizde şifrelenmiş metnin değerini arıyoruz. dönen yanıt karşılık gelen value değerinin sayısı
-    
-            $equal[] = (int)(fmod(($passvalue - $keyvalue), 26) - 1); //şifreden dönen sayı ile keyden dönen sayıyı toplayıp 26'ya göre modunu alıyoruz ve integer a çevirip $equal dizisine ekliyoruz.
+            $item = $passvalue - $keyvalue;
+            if($item <= 0){
+                $item = 26 - abs($item);
+            }
+            $equal[] = (int)(fmod(($item), 26)-1); //şifreden dönen sayı ile keyden dönen sayıyı toplayıp 26'ya göre modunu alıyoruz ve integer a çevirip $equal dizisine ekliyoruz.
+            print_r($item);
+            echo "<br>";
             @$pass[] = $b[$equal[$i]];//key değerinde ki harflere karşılık gelen sayıları, $b yani harfler dizimizde ki keylere denk gelen harflerle eşleştiriyoruz. Ve eşleşen harfler şifrelenmiş metnimiz oluyor
         }
     }
     $pass = (implode($pass)); //sifre dizisini string'e çeviriyoruz
-    
-
     header("Location:index.php?decryptedtext=".base64_encode($pass));
 }
